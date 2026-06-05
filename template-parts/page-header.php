482e1162407215913b6e7729beb06f6190d20f3d
odk37 <odake.kaito.0304@gmail.com>
2026-06-06 04:50:16 +0900

    page-header__contentの子要素クラス名を修正

--- template-parts/page-header.php
+++ template-parts/page-header.php
@@ -7,17 +7,17 @@ $page_header_content = get_field('page_header_content');
         <?php if (!empty($page_header_content)): ?>
             <div class="page-header__content">
                 <?php if (!empty($page_header_content["label"])): ?>
-                    <p class="page-header__label u-fade-in-up">
+                    <p class="page-header__content-label u-fade-in-up">
                         <?= esc_html($page_header_content["label"]); ?>
                     </p>
                 <?php endif; ?>
                 <?php if (!empty($page_header_content["heading"])): ?>
-                    <h1 class="page-header__heading u-fade-in-up">
+                    <h1 class="page-header__content-heading u-fade-in-up">
                         <?= esc_html($page_header_content["heading"]); ?>
                     </h1>
                 <?php endif; ?>
                 <?php if (!empty($page_header_content["lead"])): ?>
-                    <p class="page-header__lead u-fade-in-up">
+                    <p class="page-header__content-lead u-fade-in-up">
                         <?= nl2br(esc_html($page_header_content["lead"])); ?>
                     </p>
                 <?php endif; ?>
